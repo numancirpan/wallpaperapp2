16de12d04b576a0c195ea56e0ef64b90b8917d44
Numan Çırpan <numanccirpan@gmail.com>
2026-05-02 16:18:18 +0300

    Add Firebase Storage dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,6 +75,7 @@ dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
+    implementation(libs.firebase.storage)
     implementation("com.google.mlkit:image-labeling:17.0.9")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
