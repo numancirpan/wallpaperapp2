cc347b909ba6ba0273ba9ed8b6c3ed99be053766
Numan Çırpan <numanccirpan@gmail.com>
2026-05-03 01:53:44 +0300

    Add missing Lottie dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,7 @@ dependencies {
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.storage)
+    implementation("com.airbnb.android:lottie:6.6.2")
     implementation("com.google.mlkit:image-labeling:17.0.9")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
